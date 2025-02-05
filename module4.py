6c781b182145e2a4a80dbcc2c5259a0f5b57a341
bzhou11 <122771714+bzhou11@users.noreply.github.com>
2025-02-05 00:08:36 -0800

    add module4

--- module4.py
+++ module4.py
@@ -1,18 +1,24 @@
 def main():
     print("Hello! Welcome!")
 
-    stop_enter_number = False
     input_number_list = list()
 
-    while stop_enter_number == False:
-    	user_input = input("Please enter a number N one by one, press Enter after the number N to enter next one (Enter 'E' to stop entering and proceed with next step)\n")
 
-    	if user_input == "E": 
-    		stop_enter_number = True
-    	else:
-    		input_number_list.append(int(user_input))
+    user_input_N = input("Please enter a positive integer for input N: \n")
+    user_input_N = int(user_input_N)
+    count = 0 
+
+    print("You will provide " + str(user_input_N) + " numbers one by one, program displays the n-th number you are currently entering")
+
+    while count < user_input_N:
+        user_input = input("Enter the " + str(count+1) + " number: (only enter one number, press Enter after the number to enter next one)\n")
+        input_number_list.append(int(user_input))
+        count += 1
+
+    print("You have finished entering " + str(user_input_N) + " numbers. ")
 
     user_input_check = input("Please enter a number X to check if it exists in previous numbers you entered and generate index: \n")
+
 
     if int(user_input_check) in input_number_list:
         indexes = [i for i, x in enumerate(input_number_list) if str(x) == user_input_check]
